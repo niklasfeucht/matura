597e2ac46f7740c406082c3af711cf8230c087ad
niklasfeucht <niklas.feucht@tfeucht.at>
2023-04-27 15:25:40 +0200

    implemented basic CRUD operations over network

---
+++ NetzwerkDB/NetzwerkDB/Database.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetzwerkDB
+{
+    internal class Database
+    {
+        private static SqlConnection conn;
+
+        private static readonly string CONN_Server;
+        private static readonly string CONN_Database;
+        private static readonly string CONN_User;
+        private static readonly string CONN_Password;
+
+        static Database()
+        {
+            string[] parts;
+            string line;
+            StreamReader sr = new StreamReader("C:\\temp\\config.txt");
+
+            line = sr.ReadLine();
+            parts = line.Split('=');
+            CONN_Server = parts[1];
+            line = sr.ReadLine();
+            parts = line.Split('=');
+            CONN_Database = parts[1];
+            line = sr.ReadLine();
+            parts = line.Split('=');
+            CONN_User = parts[1];
+            line = sr.ReadLine();
+            parts = line.Split('=');
+            CONN_Password = parts[1];
+
+            sr.Close();
+        }
+
+        private Database()
+        {
+
+        }
+
+        private static SqlConnection GetSqlConnection()
+        {
+            if(conn == null)
+            {
+                conn = new SqlConnection();
+                conn.ConnectionString =
+                    "server=" + CONN_Server + ";" +
+                    "database=" + CONN_Database + ";" +
+                    "MultipleActiveResultSets=True; Integrated Security=True;";
+
+            }
+            return conn;
+        }
+
+        public static SqlConnection Connect()
+        {
+            GetSqlConnection();
+            conn.Open();
+            return conn;
+        }
+
+        public static void Disconnect()
+        {
+            if(conn!=null) conn.Close();
+        }
+
+        public static string GetSchoolDesc(int id)
+        {
+            string result = null;
+            SqlDataReader reader;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select \"desc\" from school where id=" + id + ";";
+
+            reader = cmd.ExecuteReader();
+
+            if(reader.Read())
+            {
+                result = (string)reader["desc"];
+            }
+            return result;
+        }
+
+        public static List<int> GetSchools()
+        {
+            List<int> result = new List<int>();
+            SqlDataReader reader;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select id from school;";
+
+            reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                result.Add((int)reader["id"]);
+            }
+            return result;
+        } 
+
+        public static int InsertSchool(string desc)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "insert into school(\"desc\") values ('" + desc + "');";
+
+            return cmd.ExecuteNonQuery();
+        }
+
+        public static int UpdateSchool(int id, string desc)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "update school set \"desc\"='" + desc + "' where id=" + id + ";";
+
+            return cmd.ExecuteNonQuery();
+        }
+
+        public static int DeleteSchool(int id)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "delete teaches where id_school=" + id + "; " +
+                                "delete teacher where id_school=" + id + "; " +
+                                "delete student where id_school=" + id + "; " +
+                                "delete class where id_school=" + id + "; " +
+                                "delete school where id=" + id + ";";
+
+            return cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ NetzwerkDB/NetzwerkDB/Server.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetzwerkDB
+{
+    internal class Server
+    {
+        private int count = 0;
+        public void Start()
+        {
+            IPEndPoint localEndPoint = new IPEndPoint(new IPAddress(new byte[] { 127, 0, 0, 1 }), 11111);
+            Socket listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            Socket server;
+            Database.Connect();
+
+            while (true)
+            {
+                server = listener.Accept();
+                Thread t = new Thread(new ParameterizedThreadStart(Run));
+                t.Start(server);
+            }
+            
+        }
+
+        public void Run(object o)
+        {
+            Socket server = (Socket)o;
+            int client = count++;
+
+            Console.WriteLine("Client {0} connected", client);
+
+            string msgReceived;
+            byte[] bytesReceived = new byte[1024];
+            int numBytesReceived;
+
+            string msgSend;
+            byte[] bytesSend;
+            int numBytesSend;
+
+            string cmd;
+            string table;
+            int id;
+            string desc;
+            string[] parts;
+
+            int rows=0;
+
+            do
+            {
+                numBytesReceived = server.Receive(bytesReceived);
+                msgReceived = Encoding.ASCII.GetString(bytesReceived, 0, numBytesReceived);
+                Console.WriteLine("Client {0} -> {1}", client, msgReceived);
+
+                //Protocoll
+                parts = msgReceived.Split(' ');
+                cmd = parts[0];
+                if(cmd == "get")
+                {
+                    if(parts.Length == 2)
+                    {
+                        table = parts[1];
+                        List<int> schools = Database.GetSchools();
+                        msgSend = "";
+                        foreach(int i in schools)
+                        {
+                            msgSend += Database.GetSchoolDesc(i) + ", ";
+                        }
+                    }
+                    else if(parts.Length == 3)
+                    {
+                        table = parts[1];
+                        id = int.Parse(parts[2]);
+                        msgSend = Database.GetSchoolDesc(id);
+                    }
+                    else
+                    {
+                        msgSend = "command get needs parameters <table> [id]";
+                    }
+                }
+                else if(cmd == "post")
+                {
+                    if(parts.Length > 2)
+                    {
+                        table = parts[1];
+                        if(table == "school" && parts.Length == 3)
+                        {
+                            desc = parts[2];
+                            rows = Database.InsertSchool(desc);
+                            msgSend = "inserted " + rows + " rows";
+                        }
+                        else
+                        {
+                            msgSend = "command post needs the right amount of parameters";
+                        }
+                    }
+                    else
+                    {
+                        msgSend = "command post needs parameters <table> <value> ...";
+                    }
+                }
+                else if(cmd == "put")
+                {
+                    if (parts.Length > 2)
+                    {
+                        table = parts[1];
+                        if (table == "school" && parts.Length == 4)
+                        {
+                            id = int.Parse(parts[2]);
+                            desc = parts[3];
+                            rows = Database.UpdateSchool(id, desc);
+                            msgSend = "updated " + rows + " rows";
+                        }
+                        else
+                        {
+                            msgSend = "command put needs the right amount of parameters";
+                        }
+                    }
+                    else
+                    {
+                        msgSend = "command put needs parameters <table> <value> ...";
+                    }
+                }
+                else if(cmd == "delete")
+                {
+                    if(parts.Length == 3)
+                    {
+                        table = parts[1];
+                        id = int.Parse(parts[2]);
+                        rows = Database.DeleteSchool(id);
+                        msgSend = "deleted " + rows + " rows";
+                    }
+                    else
+                    {
+                        msgSend = "command delete needs parameters <table> <id>";
+                    }
+                }
+                else if(cmd == "bye")
+                {
+                    msgSend = "goodbye";
+                }
+                else
+                {
+                    msgSend = "command is not supported";
+                }
+
+                bytesSend = Encoding.ASCII.GetBytes(msgSend);
+                numBytesSend = server.Send(bytesSend);
+
+            } while (msgReceived != "bye");
+
+            Console.WriteLine("Client {0} disconnected", client);
+
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+        }
+    }
+}
